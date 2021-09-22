1aa40aac6509d4663f065b49a4dfe03bbb6e9f65
Navi Kaur <navdeep9520@gmail.com>
2021-09-22 16:35:20 -0700

    commit tree 7563ba7e5d39f85c2d3d98d4e50278e97946df8b

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = `bob`;
+// console.log(customerName);
+ 
+// function upperCaseCustomerName(customerName) {
+//     return customerName.toUpperCase();
+// }
+
+// let customerName = `bob`;
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+// upperCaseCustomerName();
+// console.log(customerName);
+
+// function setBestCustomer() {
+//   let bestCustomer = `not bob`;
+//   return bestCustomer;
+// }
+ 
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+  return bestCustomer;
+}
